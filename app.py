aa05260b23f41b7db0e1977759ad11ad4956ad5a
Hamza Elashry <98109755+rechanting@users.noreply.github.com>
2024-11-04 23:27:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,28 +4,9 @@ import bcrypt
 import pymongo
 import os
 import markdown
-import json
 from groq import Groq
 
-#! Recaptcha
-class ReCaptcha():
-    SECRET_KEY = 'jhafyBEYDHBF*fhu0_Sd;aspd#Y&*G' 
-    RECAPTCHA_USE_SSL= False
-    RECAPTCHA_PUBLIC_KEY = '6LdjPC8qAAAAAMeH4k8TvOR4F1cWWYFxaxNhSRdw' # google generated
-    RECAPTCHA_PRIVATE_KEY = '6LdjPC8qAAAAAHPELHEQE8MnzI8Z7QEZwuxxHX03' # google generated
-    RECAPTCHA_DATA_ATTRS = {'theme': 'light'}
-
-def is_human(captcha_response):
-    payload = {'response': captcha_response, 'secret': private_key}
-    response = post("https://www.google.com/recaptcha/api/siteverify", data=payload)
-    response_text = json.loads(response.text)
-    return response_text['success']
-
-pub_key = ReCaptcha.RECAPTCHA_PUBLIC_KEY
-private_key = ReCaptcha.RECAPTCHA_PRIVATE_KEY
-
 app = Flask(__name__)
-app.config.from_object(ReCaptcha)
 app.secret_key = "jhafyBEYDHBF*fhu0_Sd;aspd#Y&*G"
 
 # Database
@@ -54,7 +35,6 @@ def register():
     message = ''
     if 'email' in session:
         return redirect(url_for('dashboard'))
-    
     if request.method == 'POST':
         user = request.form.get("name")
         email = request.form.get('email')
@@ -73,13 +53,9 @@ def register():
             return render_template('register.html', message=message)
         
         if password != passwordAgain:
-            message = 'Passwords should match! Try again.'
+            message = 'Passwords should match! try again.'
             return render_template('register.html', message=message)
-        
-        captcha_response = request.form['g-recaptcha-response']
-        print(captcha_response)
-        if is_human(captcha_response):
-            # If ReCaptcha passes, proceed with hashing the password and registering the user
+        else:
             hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
             user_input = {'name': user, 'email': email, 'password': hashed}
             auth.insert_one(user_input)
@@ -88,13 +64,9 @@ def register():
             session["email"] = email
             session["name"] = user
             return redirect(url_for('dashboard'))
-        else:
-            # If ReCaptcha fails, return an error message
-            message = 'ReCaptcha failed, please try again.'
-            return render_template('register.html', message=message)
     
-    return render_template('register.html', pub_key=pub_key)
-    
+    return render_template('register.html')
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     message = 'Please login to your account'
@@ -105,14 +77,7 @@ def login():
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
-        recaptcha_response = request.form.get('g-recaptcha-response')
 
-        # Verify reCAPTCHA
-        if not is_human(recaptcha_response):
-            message = 'reCAPTCHA verification failed. Please try again.'
-            return render_template('login.html', message=message, pub_key=pub_key)
-
-        # Check email and password
         email_found = auth.find_one({"email": email})
         if email_found:
             passwordcheck = email_found['password']
@@ -123,12 +88,12 @@ def login():
                 return redirect(url_for('dashboard'))
             else:
                 message = 'Wrong password'
-                return render_template('login.html', message=message, pub_key=pub_key)
+                return render_template('login.html', message=message)
         else:
             message = 'Email not found'
-            return render_template('login.html', message=message, pub_key=pub_key)
+            return render_template('login.html', message=message)
     
-    return render_template('login.html', pub_key=pub_key)
+    return render_template('login.html')
 
 @app.route('/signout')
 def signout():
